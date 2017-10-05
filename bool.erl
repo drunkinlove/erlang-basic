076b72a1c3cbdb9830b1e3ee662519930b9e7f5b
Nick <31738272+drunkinlove@users.noreply.github.com>
2017-10-05 14:36:48 +0300

    Add files via upload

---
+++ bool.erl
@@ -0,0 +1,15 @@
+-module(bool).
+
+-export([b_not/1, b_and/2, b_or/2, b_xor/2]).
+
+b_not(false) -> true;
+b_not(true) -> false.
+
+b_and(false, false) -> false;
+b_and(_,_) -> true.
+
+b_or(true, true) -> true;
+b_or(_,_) -> false.
+
+b_xor(X, X) when X =:= X -> false;
+b_xor(_,_) -> true.

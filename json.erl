cf23b4bb5d60d3531ea155c3db5a053533c0c915
Nick <31738272+drunkinlove@users.noreply.github.com>
2017-10-11 18:45:51 +0300

    Add files via upload

--- json.erl
+++ json.erl
@@ -1,13 +1,22 @@
--module(json).
-
-
--export([new/2, read/1]).
+-module(json).E
+-export([new/2, read/2, write/3]).
 
 
 new(Key, Value) -> 
-	Jsons = #{},
-	Jsons#{Key => Value}.
+	#{Key => Value}.		
 
-read(Key) -> 
-	#{Key := X} = Jsons,
-	X.
+read(Key, JsonObj) -> 
+	try #{Key := _} = JsonObj of
+		_ -> 
+		#{Key := Value} = JsonObj,
+		{ok, Value}
+	catch
+		error:{badmatch, _} -> {error, not_found}
+	end.
+	
+write(Key, Value, JsonObj) -> 
+	try JsonObj#{Key := Value} of
+		_ -> JsonObj
+	catch
+		error:{badkey, _} -> {error, not_found}
+	end.

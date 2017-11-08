61f27de4f95b4d94cdd6f9dfd78c862fdecdfbeb
drunkinlove <dontknowwrongfromright@yandex.ru>
2017-11-08 17:39:00 +0300

    ok now this looks more like it

--- atm.erl
+++ atm.erl
@@ -1,46 +1,113 @@
 -module(atm).
+-behavior(gen_statem).
+-compile(export_all).
 
-start_link([{CardNo, Pin, Balance}]) ->
-	case gen_server:start_link(?MODULE, [], [])) of
+
+callback_mode() -> state_functions.
+
+name() -> atm.
+
+
+
+%%% API
+
+start_link(Cards) ->
+	case gen_statem:start_link(?MODULE, {Cards, none, []}, []) of
 		{ok, Pid} ->
-			ok;
+			case register(name(), Pid) of
+				true -> ok;
+				{error, E} -> {error, E}
+			end;
 		{error, E} ->
 			{error, E}
 	end.
 
 insert_card(CardNo) ->
-	gen_server:call(Atm, {inserted, CardNo}).
+	gen_statem:call(name(), {inserted, CardNo}).
 
 push_button(Button) ->
-	gen_server:call(Atm, {pressed, Button}).
+	gen_statem:call(name(), {pressed, Button}).
 
-%%% Server functions
 
-init(Cards) -> {ok, Cards]}.
 
-receive() -> string.
+%%% Callback functions
 
-handle_call({inserted, CardNo}, _From, Cards) ->
-	case [receive()] of
-		[Password || {CardNo, Password, _} <- Cards] -> 
-			case [Sum - receive() || {CardNo, _, Sum} <- Cards] of
-				[NewSum] when NewSum >= 0 ->
-					{reply, {"here's your money"}, [{CardNo, Password, NewSum}] ++ Cards -- [{CardNo, Password, Sum}]};
-				[NewSum] ->
-					{reply, {"not allowed"}, Cards};
-				{error, E} ->
-					{reply, E, Cards}
-		[cancel] ->
-			{reply, "canceled", Cards};
-		[_] ->
-			{reply, "invalid password", Cards};
-		{error, E} ->
-			{reply, E, Cards} 
+
+init({Cards, CurrentNo, Input}) -> 
+	State = wait,
+	io:fwrite("This ATM is currently operable.~n"),
+	{ok, State, {Cards, CurrentNo, Input}}.
+
+wait({call, From}, {inserted, CardNo}, {Cards, CurrentNo, Input}) ->
+	{next_state, check_card, {Cards, CardNo, Input}};
+wait(internal, wait, {Cards, CurrentNo, Input}) ->
+	{next_state, wait, {Cards, CurrentNo, Input}}.
+
+check_card(internal, check_card, {Cards, CurrentNo, Input}) ->
+	case [X || {X, _, _} <- Cards, X =:= CurrentNo] of
+		[X] -> {next_state, wait_for_pin, {Cards, CurrentNo, Input}};
+		[] ->
+			io:fwrite("Sorry, unrecognized card. Returning to initial screen...~n"),
+			{next_state, wait, {Cards, none, []}};
+		_ ->
+			io:fwrite("An error has occured. Returning to initial screen...~n"),
+			{next_state, wait, {Cards, none, []}}
 	end.
 
-handle_call({pressed, Button}, _From, Cards) ->
-	case Button of
-		cancel ->
-			{reply, Cancel, Cards};
-		Number when Number >= 0 andalso Number <= 9:
-			
+wait_for_pin(internal, wait_for_pin, {Cards, CurrentNo, Input}) ->
+	io:fwrite("Waiting for input...~n"),
+	{next_state, wait_for_pin, {Cards, CurrentNo, Input}};
+wait_for_pin({call, From}, {pressed, Button}, {Cards, CurrentNo, Input}) ->
+	case lists:member(Button, ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']) of
+		true ->
+			{next_state, wait_for_pin, {Cards, Input ++ atom_to_list(Button)}};
+		false when Button =:= enter ->
+			{next_state, check_pin, {Cards, string:to_integer(Input)}};
+		false when Button =:= cancel ->
+			io:fwrite("Canceled by user. Returning to initial screen...~n"),
+			{next_state, wait, {Cards, none, []}}
+	end.
+
+check_pin({call, From}, {inserted, CardNo}, {Cards, CurrentNo, Input}) ->
+	case [Input] =:= [Y || {X, Y, _} <- Cards, X =:= CardNo] of
+		true ->
+			io:fwrite("Correct pin. Enter sum you want to withdraw:~n"),
+			{next_state, wait_for_sum, {Cards, []}};
+		_ ->
+			io:fwrite("Wrong pin, sorry. Try again~n"),
+			{next_state, wait_for_pin, {Cards, CurrentNo, []}}
+	end.
+
+wait_for_sum(internal, wait_for_sum, {Cards, CurrentNo, Input}) ->
+	io:fwrite("Waiting for input...~n"),
+	{next_state, wait_for_sum, {Cards, CurrentNo, Input}};
+wait_for_sum({call, From}, {pressed, Button}, {Cards, CurrentNo, Input}) ->
+	case lists:member(Button, ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']) of
+		true ->
+			{next_state, wait_for_sum, {Cards, CurrentNo, Input ++ atom_to_list(Button)}};
+		false when Button =:= enter ->
+			{next_state, check_sum, {Cards, CurrentNo, string:to_integer(Input)}};
+		false when Button =:= cancel ->
+			io:fwrite("Canceled by user. Returning to initial screen...~n"),
+			{next_state, wait, {Cards, none, []}}
+	end.
+
+check_sum(internal, check_sum, {Cards, CurrentNo, Input}) ->
+	case [Z - Input || {X, Y, Z} <- Cards, X =:= CurrentNo] of
+		[NewSum] when NewSum >= 0 ->
+			io:fwrite("Here's your money. Thank you~n"),
+			[{Pin, OldSum}] = [{Y, Z} || {X, Y, Z} <- Cards, X =:= CurrentNo],
+			{next_state, wait, {[{CurrentNo, Pin, NewSum}] ++ Cards -- [{CurrentNo, Pin, OldSum}], none, []}};
+		[NewSum] ->
+			io:fwrite("Insufficent balance, sorry. Try again~n"),
+			{next_state, wait_for_sum, {Cards, CurrentNo, []}};
+		_ ->
+			io:fwrite("An error has occured. Returning to initial screen...~n"),
+			{next_state, wait, {Cards, none, []}}	
+		end.
+
+
+
+%%% Dummy functions
+
+return_card() -> ok.
